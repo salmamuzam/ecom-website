62fea3f834956ad83576404b2421edee763b5237
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-08 17:29:55 +0530

    Customized JetStream Profile

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-hidden focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center justify-center px-4 py-2 bg-[#822659] border border-transparent rounded-md font-semibold text-xs text-[#F0F0F0] uppercase tracking-widest hover:bg-[#004D61] hover:text-[#F0F0F0] active:bg-[#3E5641] active:text-[#F0F0F0] focus:outline-none focus:ring-2 focus:ring-[#004D61] focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
