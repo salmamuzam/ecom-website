4a21f30e37e5fb2f40b36c4da059c63c4dc763d2
Fathima Salma Muzammil <salmamuzam77@gmail.com>
2025-12-09 19:24:22 +0530

    Added User Role

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'first_name',
         'last_name',
         'username',
+        'user_role',
         'email',
         'password',
     ];
